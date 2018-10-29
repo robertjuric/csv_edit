20352237461673c51832718246f0f047f33084e1
Robert Juric <robert.juric@gmail.com>
2018-10-29 14:38:43 -0500

    Added input validation

--- replace.py
+++ replace.py
@@ -1,24 +1,46 @@
 import sys
 import csv
+import os.path
 from os.path import dirname
+import re
 
 new_rows = []
 #changes will search within strings
 #need to be add regex to ensure beginning and end
 
-#Need to add input validation for number of arguments
-#Need to add input validation for filetype
-filename = str(sys.argv[1])
-original_dir = dirname(filename)
-modfile = original_dir + '/modified.csv'
+#Input validation for number of arguments
+if len(sys.argv) < 4:
+	print "Incorrect syntax"
+	print "Correct syntax: replace.py file.csv search_string replace_string"
+	sys.exit(2)
+
+
+original_file = str(sys.argv[1])
+print "Working with file: ", original_file
+#Regex search for ends with '.csv'
+p = re.compile('\.csv\Z')
+m = p.search(original_file)
+if m:
+	#File ends in .csv
+	#Check if file actually exists
+	if not os.path.exists(original_file):
+		print "File does not exist"
+		sys.exit()
+else:
+	print "File listed is not a CSV file"
+	sys.exit()
+
+original_dir = dirname(original_file)
+print "Path is: ", original_dir
+
+mod_file = original_dir + '/modified.csv'
+print "New file is: ", mod_file
+
 changes = {
 	str(sys.argv[2]) : str(sys.argv[3]),
 	}
-print "Working with file: ", filename
-print "Path is: ", original_dir
-print "New file is: ", modfile
 
-with open (filename, 'rb') as f:
+with open (original_file, 'rb') as f:
 	reader = csv.reader(f)
 	for row in reader:
 		new_row = row
@@ -27,6 +49,6 @@ with open (filename, 'rb') as f:
 		new_rows.append(new_row)
 
 #possible to open same file, but I want to keep original intact
-with open(modfile, 'wb') as f:
+with open(mod_file, 'wb') as f:
 	writer = csv.writer(f,)
 	writer.writerows(new_rows)

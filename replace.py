4d758fb355f65716aa41acb69073e14b9a88f37d
Robert Juric <robert.juric@gmail.com>
2018-10-29 16:06:18 -0500

    Cleaned up code and added comments.

--- replace.py
+++ replace.py
@@ -4,19 +4,21 @@ import os.path
 from os.path import dirname
 import re
 
+#Some Variables
+original_file = str(sys.argv[1])
+original_dir = dirname(original_file)
+mod_file = original_dir + '/modified.csv'
+changes = {
+	str(sys.argv[2]) : str(sys.argv[3]),
+	}
 new_rows = []
-#changes will search within strings
-#need to be add regex to ensure beginning and end
 
 #Input validation for number of arguments
 if len(sys.argv) < 4:
-	print "Incorrect syntax"
+	print "Incorrect syntax."
 	print "Correct syntax: replace.py file.csv search_string replace_string"
 	sys.exit(2)
-
-
-original_file = str(sys.argv[1])
-print "Working with file: ", original_file
+#Input validation for the file
 #Regex search for ends with '.csv'
 p = re.compile('\.csv\Z')
 m = p.search(original_file)
@@ -24,31 +26,30 @@ if m:
 	#File ends in .csv
 	#Check if file actually exists
 	if not os.path.exists(original_file):
-		print "File does not exist"
+		print "File submitted does not exist"
 		sys.exit()
 else:
-	print "File listed is not a CSV file"
+	print "Filename submitted is not a CSV file"
 	sys.exit()
 
-original_dir = dirname(original_file)
-print "Path is: ", original_dir
-
-mod_file = original_dir + '/modified.csv'
-print "New file is: ", mod_file
-
-changes = {
-	str(sys.argv[2]) : str(sys.argv[3]),
-	}
-
+#Open original file
+print "Opening file..."
 with open (original_file, 'rb') as f:
 	reader = csv.reader(f)
+	#Loop through each row
 	for row in reader:
 		new_row = row
+		#Search and replace values
 		for key, value in changes.items():
 			new_row = [ x.replace(key, value) for x in new_row ]
+		#Write new values into array
 		new_rows.append(new_row)
 
-#possible to open same file, but I want to keep original intact
+#Open new modified file
+#Possible to open same file, but I want to keep original intact
+print "Writing new file..."
 with open(mod_file, 'wb') as f:
 	writer = csv.writer(f,)
+	#Write new_rows array into file
 	writer.writerows(new_rows)
+print "Complete."
